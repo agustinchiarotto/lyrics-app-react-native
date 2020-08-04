d6631e81ea0d4d77fad96403c6637ba362b26d02
Edgar Andres Bonilla Grajales <edgar.a.bonilla.g@gmail.com>
2020-08-04 00:28:08 -0500

    Added Icons and custom style to the main bottom tab bar.
    * Integrated react-native-vector-icons.
    * Used react-native-vector-icons to show specific icons on each tab.

--- src/navigation/MainTabNavigator.tsx
+++ src/navigation/MainTabNavigator.tsx
@@ -1,12 +1,49 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { RouteProp } from '@react-navigation/native';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 import { HistoryScreen, SearchScreen } from '../screens';
 
 const Tab = createBottomTabNavigator();
 
+type Route = RouteProp<Record<string, object | undefined>, string>;
+
+const getIconName = (routeName: string) => {
+  let iconName = '';
+  switch (routeName) {
+    case 'Search':
+      iconName = 'search';
+      break;
+    case 'History':
+      iconName = 'history';
+      break;
+    default:
+      iconName = 'help';
+      break;
+  }
+  return iconName;
+};
+
+const navigatorScreenOptions = ({ route }: { route: Route }) => ({
+  tabBarIcon: ({ focused, color, size }: { focused: boolean; color: string; size: number }) => {
+    const iconName = getIconName(route.name);
+    const iconSize = focused ? size * 1.25 : size;
+    return <MaterialIcon name={iconName} size={iconSize} color={color} />;
+  },
+});
+
+const navigatorTabBarOptions = {
+  allowFontScalling: false,
+  activeTintColor: 'tomato',
+  inactiveTintColor: 'gray',
+  labelStyle: {
+    fontSize: 12,
+  },
+};
+
 const MainTabNavigator = () => (
-  <Tab.Navigator>
+  <Tab.Navigator screenOptions={navigatorScreenOptions} tabBarOptions={navigatorTabBarOptions}>
     <Tab.Screen name="Search" component={SearchScreen} />
     <Tab.Screen name="History" component={HistoryScreen} />
   </Tab.Navigator>
